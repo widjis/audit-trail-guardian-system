4d6a1221430a89f571780592f944fcc11c9a374a
widjis <widji.santoso@gmail.com>
2025-06-21 06:02:46 +0800

    feat: integrate ldap search in user info

--- tests/ldapQuery.test.js
+++ tests/ldapQuery.test.js
@@ -3,18 +3,14 @@ import assert from 'node:assert/strict';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { search } from '../src/server/lib/ldapService.js';
+import { getAdUserInfo } from '../src/server/routes/active-directory.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const settingsPath = path.join(__dirname, '../src/server/data/settings.json');
 const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
-const baseDN = settings.activeDirectorySettings.baseDN;
-
-const filter = '(&(objectClass=user)(sAMAccountName=*widji.santoso*))';
-
-test('ldap query returns entries', async () => {
-  const entries = await search(baseDN, filter, ['sAMAccountName', 'displayName']);
-  console.log(entries);
-  assert.ok(Array.isArray(entries));
+test('getAdUserInfo retrieves data', async () => {
+  const info = await getAdUserInfo(settings.activeDirectorySettings, 'widji.santoso');
+  console.log(info);
+  assert.equal(typeof info, 'object');
 });
